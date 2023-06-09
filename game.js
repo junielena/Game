447492c57b912712873e36be1dbd09f7ff353e6f
junielena <93175266+junielena@users.noreply.github.com>
2023-06-08 18:50:34 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1,65 +1,68 @@
 const players = ['X', 'O'];
 let currentPlayer = players[0];
 const squares = document.querySelectorAll('.square');
+const restartButton = document.querySelector('#clear-button');
 
-
-function handleClick(event) {
-    const square = event.target;
-    square.textContent = currentPlayer;
-    currentPlayer = currentPlayer === 'X' ? 'O': 'X';
-    if(checkWinningCondition()){
-        alert('${currentPlayer} won!');
-    }
-    
-    currentPlayer = currentPlayer ===
-    players[0] ? players[1] : players[0];
+function handleCtick(event) {
+  const clickedSquare = event.target;
+  clickedSquare.textContent = currentPlayer;
+  clickedSquare.removeEventListener('click', handleCtick);
+  checkForWinner();
+  switchPlayers();
 }
 
-function clearBoard(){
-    squares.forEach(square =>
-        square.textContent ='');
+function switchPlayers() {
+  currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
 }
-
-
-squares.forEach(square =>
-    square.addEventListener('click', handleClick)
-    );
-  
 
 function checkForWinner() {
-    const squares =
-    Array.from(document.querySelectorAll('.square'));
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
 
-  const winningConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-
-
-  for (let i = 0; i < winningCombination.length; i++) {
-    const [a, b, c] = winningCombination[i];
-    if (squares[a].textContent === 
-        currentPlayer &&
-        squares[b].textContent ===
-        currentPlayer &&
-        squares[c].textContent ===
-        currentPlayer){
-            return true;
-
-        } 
-        
+let isDraw = true
+for (let i = 0; i <squares.length; i++){
+    if(squares[i].textContent ===''){
+        isDraw = false;
+        break;
     }
-
-  return false;
 }
 
+if (isDraw){
+    alert('Draw!');
+    return;
+}
 
-const clearButton =
-document.querySelector('#clear-button');
-clearButton.addEventListener('click', clearBoard);
+  for (let i = 0; i < winningCombinations.length; i++) {
+    const [a, b, c] = winningCombinations[i];
+    if (
+      squares[a].textContent === currentPlayer &&
+      squares[b].textContent === currentPlayer &&
+      squares[c].textContent === currentPlayer
+    ) {
+      alert(`${currentPlayer} wins!`);
+      restartGame();
+    }
+  }
+}
+
+function restartGame() {
+  squares.forEach(square => {
+    square.textContent = '';
+    square.addEventListener('click', handleCtick, { once: true });
+  });
+  currentPlayer = players[0];
+}
+
+squares.forEach(square => {
+  square.addEventListener('click', handleCtick, { once: true });
+});
+
+restartButton.addEventListener('click', restartGame);
